04c0054915974f0c7c2bf687799339477cb926ea
alexarevalo-tele <aaa00081@red.ujaen.es>
2026-02-11 19:33:01 +0100

    Añadir servicio POST, de email y contraseña

--- src/main/java/com/example/demo/Controlador.java
+++ src/main/java/com/example/demo/Controlador.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @Controller
 public class Controlador {
@@ -10,6 +13,16 @@ public class Controlador {
     public String vistaprincipal(Model m){
         m.addAttribute("mensaje", "Esta es la vista pricipal");
         return "vista";
+    }
+
+    @PostMapping(value = "/datosusuario")
+    public String metodo(HttpServletRequest req) {
+    String email = req.getParameter("email");
+    String nombre = req.getParameter("nombre");
+    
+    System.out.println(email + ", " + nombre);
+    
+    return "vista";
     }
     
 }
